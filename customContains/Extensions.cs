c125419bac69ea3fd30b77c8542f0dcc1c94cb37
Ayxan23 <ayxan042223@gmail.com>
2022-12-07 03:25:39 +0400

    customContains

---
+++ customContains/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customContains
+{
+    internal static class Extensions
+    {
+
+        public static bool customContains(this string word, string str)
+        {
+            int index = 0;
+            string test = "";
+            bool result = false;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (test.Length == str.Length)
+                {
+                    result = true;
+                    break;
+                }   
+                else if (word[i] == char.ToUpper(str[index]) || word[i] == char.ToLower(str[index]))
+                {
+                    test += word[i];
+                    if (test.Length != str.Length)
+                    {
+                        index++;
+                    }
+                }
+                else if (test.Length != str.Length)
+                {
+                    index = 0;
+                    test = "";
+                }
+            }
+            return result;            
+        }
+
+    }
+}
---
+++ customContains/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace customContains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sentence = "Bu bir bur custom"; // custom contains methodudur
+
+            Console.WriteLine(sentence.customContains("bur"));
+
+        }
+    }
+}
